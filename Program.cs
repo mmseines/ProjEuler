a09bca96cca5d37743404d3c3a110a9e224c2769
mmseines <mmseines@gmail.com>
2018-04-23 16:24:12 +0200

    oops

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace ProjEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proj_euler_class1 p = new Proj_euler_class1();
+            Proj_euler_class2 p2 = new Proj_euler_class2();
+            Proj_euler_class3 p3 = new Proj_euler_class3();
+            /*
+            System.Console.WriteLine("Solution problem 1:");
+            p.Solution_problem_one(1000);
+            System.Console.WriteLine("Solution problem 2:");
+            p.Solution_problem_two(4000000);
+            
+            System.Console.WriteLine("Solution problem 3:");
+            p.Solution_problem_three(600851475143);
+            System.Console.WriteLine("Solution problem 4:");
+            p.Solution_problem_four();
+
+            System.Console.WriteLine("Solution problem 5:");
+            p.Solution_problem_five(20);
+
+            System.Console.WriteLine("Solution problem 6:");
+            p.Solution_problem_six(100);
+            
+            System.Console.WriteLine("Solution problem 7:");
+            p.Solution_problem_seven(10001);
+
+            System.Console.WriteLine("Solution problem 8:");
+            p.Solution_problem_eight(13);
+
+            System.Console.WriteLine("Solution problem 9:");
+            p.Solution_problem_nine(1000);
+
+            System.Console.WriteLine("Solution problem 10:");
+            p.Solution_problem_ten(2000000);
+
+            System.Console.WriteLine("Solution problem 11:");
+            p.Solution_problem_eleven();
+            
+         
+            System.Console.WriteLine("Solution problem 12:");
+            p2.Solution_problem_12(500);
+
+            System.Console.WriteLine("Solution problem 13:");
+            p2.Solution_problem_13("C:\\Users\\lab\\source\\repos\\ConsoleApp1\\ConsoleApp1\\Problem13.txt");
+
+            System.Console.WriteLine("Solution problem 14:");
+            p2.Solution_problem_14(1000000);
+
+            System.Console.WriteLine("Solution problem 15:");
+            p2.Solution_problem_15(20);
+
+            System.Console.WriteLine("Solution problem 16:");
+            p2.Solution_problem_16(1000);
+
+            System.Console.WriteLine("Solution problem 17:");
+            p2.Solution_problem_17();
+
+            System.Console.WriteLine("Solution problem 18:");
+            p2.Solution_problem_18("C:\\Users\\lab\\source\\repos\\ConsoleApp1\\ConsoleApp1\\Problem18.txt");
+            System.Console.WriteLine("Solution problem 67:");
+            p2.Solution_problem_18("C:\\Users\\lab\\source\\repos\\ConsoleApp1\\ConsoleApp1\\Problem67.txt");
+
+            System.Console.WriteLine("Solution problem 19:");
+            p2.Solution_problem_19(1901,2000);
+            
+            System.Console.WriteLine("Solution problem 20:");
+            p2.Solution_problem_20(100);
+
+            System.Console.WriteLine("Solution problem 21:");
+            p2.Solution_problem_21(100000);
+
+            System.Console.WriteLine("Solution problem 22:");
+            p2.Solution_problem_22("C:\\Users\\lab\\source\\repos\\ConsoleApp1\\ConsoleApp1\\Problem22.txt");
+
+            System.Console.WriteLine("Solution problem 23:");
+            p2.Solution_problem_23();
+            */
+            System.Console.WriteLine("Solution problem 24:");
+            p3.Solution_problem_24(1000000);
+
+            System.Console.WriteLine("Solution problem 25:");
+            p3.Solution_problem_25(1000);
+
+            System.Console.WriteLine("Solution problem 26:");
+            p3.Solution_problem_26(1000);
+
+            System.Console.WriteLine("Solution problem 27:");
+            p3.Solution_problem_27(1000, 1000);
+
+            System.Console.WriteLine("Solution problem 28:");
+            p3.Solution_problem_28(1001);
+
+            System.Console.WriteLine("Solution problem 29:");
+            p3.Solution_problem_29(100,100);
+
+            System.Console.WriteLine("Solution problem 30:");
+            p3.Solution_problem_30();
+
+            System.Console.WriteLine("Solution problem 31:");
+            p3.Solution_problem_31(200);
+
+            System.Console.WriteLine("Solution problem 32:");
+            p3.Solution_problem_32();
+
+            System.Console.WriteLine("Solution problem 33:");
+            p3.Solution_problem_33();
+
+            System.Console.WriteLine("Solution problem 34:");
+            p3.Solution_problem_34();
+
+            System.Console.WriteLine("Solution problem 35:");
+            p3.Solution_problem_35(1000000);
+
+            System.Console.WriteLine("Solution problem 36:");
+            p3.Solution_problem_36(1000000);
+
+            System.Console.WriteLine("Solution problem 187:");
+            p3.Solution_problem_187(100000000);
+
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
